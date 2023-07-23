80feb934e9ca5e747f4e9b8978c7910e3488e285
DylanHalstead <dylanhalstead11@gmail.com>
2023-07-23 14:47:32 -0400

    discord.js setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,14 +7,11 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:eslint-comments/recommended',
-    'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
     'prettier',
   ],
-  plugins: ['@typescript-eslint', 'node', 'prettier'],
-  parser: '@typescript-eslint/parser',
+  plugins: ['node', 'prettier'],
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
@@ -40,34 +37,6 @@ module.exports = {
     'no-await-in-loop': ['error'],
     'comma-dangle': ['error', 'always-multiline'],
     'eslint-comments/no-unused-disable': 'warn',
-    '@typescript-eslint/no-shadow': 'error',
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      {
-        args: 'after-used',
-        ignoreRestSiblings: true,
-        vars: 'all',
-      },
-    ],
-    '@typescript-eslint/type-annotation-spacing': [
-      'error',
-      {
-        after: true,
-        before: true,
-        overrides: {
-          colon: {
-            before: false,
-          },
-        },
-      },
-    ],
-    '@typescript-eslint/naming-convention': [
-      'error',
-      {
-        format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
-        selector: 'variable',
-      },
-    ],
   },
   settings: {
     'import/resolver': {
--- app.ts
+++
@@ -1,5 +0,0 @@
-import 'dotenv/config';
-import { Client, Message, TextChannel } from 'discord.js';
-import dotenv from 'dotenv';
-
-dotenv.config();
